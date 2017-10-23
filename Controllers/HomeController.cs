519fe9cf57d3d2504411fec143ac72b0e32d327d
pasaunders <kaorti@gmail.com>
2017-10-23 16:10:21 -0700

    pre testing commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IActionResult Result(string name, string location, string language, string comments)
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = language;
+            ViewBag.Comments = comments;
+
+            return View();
+        }
+    }
+}
